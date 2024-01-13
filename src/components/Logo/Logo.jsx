18cc50e01acd17435795e1f8ea17d45547423ac9
mary-ue <maryandreeva100@gmail.com>
2024-01-14 00:38:50 +0300

    header

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,12 @@
+import logoSvg from './logo.svg';
+import s from './Logo.module.scss';
+
 export const Logo = () => {
 
 
   return (
-    <div>
-      Logo
-    </div>
+    <a className={s.link} href="/">
+      <img className={s.img} src={logoSvg} alt="Логотип мебельного маркета Koff" />
+    </a>
   )
 }
