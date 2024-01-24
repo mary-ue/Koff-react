b39adfe0acf9e6896301f021cb84b37eb44947c0
mary-ue <maryandreeva100@gmail.com>
2024-01-25 00:12:31 +0300

    add pagination

--- src/views/Goods/Goods.jsx
+++ src/views/Goods/Goods.jsx
@@ -5,28 +5,30 @@ import s from './Goods.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchGoods } from '../../store/goods/goods.slice';
 import { useSearchParams, useLocation } from 'react-router-dom';
+import { Pagination } from '../../components/Pagination/Pagination';
 
 export const Goods = () => {
   const dispatch = useDispatch();
   const [searchParam] = useSearchParams();
-  const { data: goods, loading, error } = useSelector((state) => state.goods);
+  const { data: goods, loading, error, pagination } = useSelector((state) => state.goods);
   const { favoriteList } = useSelector((state) => state.favorite);
   const { pathname } = useLocation();
 
   const category = searchParam.get('category');
   const q = searchParam.get('q');
+  const page = searchParam.get('page');
 
   useEffect(() => {
     if (pathname !== '/favorite') {
-      dispatch(fetchGoods({ category, q }));
+      dispatch(fetchGoods({ category, q, page }));
     }
-  }, [dispatch, category, q, pathname]);
+  }, [dispatch, category, q, pathname, page]);
 
   useEffect(() => {
     if (pathname === '/favorite') {
-      dispatch(fetchGoods( { list: favoriteList.join(',') }));
+      dispatch(fetchGoods({ list: favoriteList.join(','), page }));
     }
-  }, [dispatch, favoriteList, pathname]);
+  }, [dispatch, favoriteList, pathname, page]);
 
   if (loading) return <div>Загрузка...</div>;
 
@@ -37,14 +39,17 @@ export const Goods = () => {
       <Container>
         <h2 className={`${s.title} visually-hidden`}>Список товаров</h2>
         {goods.length ? (
-          <ul className={s.list}>
-            {goods &&
-              goods.map((good) => (
-                <li key={good.id}>
-                  <CardItem {...good} />
-                </li>
-              ))}
-          </ul>
+          <>
+            <ul className={s.list}>
+              {goods &&
+                goods.map((good) => (
+                  <li key={good.id}>
+                    <CardItem {...good} />
+                  </li>
+                ))}
+            </ul>
+            {pagination ? <Pagination pagination={pagination} /> : ''}
+          </>
         ) : (
           <p>По вашему запросу товаров не найдено</p>
         )}
